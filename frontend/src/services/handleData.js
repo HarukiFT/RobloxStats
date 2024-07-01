8273887d7299c1c40367b0d49a95f9f325201681
Haruki <aratashiharuki@gmail.com>
2024-07-01 19:26:50 +0300

    Changed direct download from handleData to only return blob

--- frontend/src/services/handleData.js
+++ frontend/src/services/handleData.js
@@ -74,13 +74,8 @@ class DataSet {
         });
 
         const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-        
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.setAttribute('download', 'data.csv');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+
+        return blob
     }
 }
 
