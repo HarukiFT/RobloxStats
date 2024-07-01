ec6ba041658e304a23a72e53b884b22d49a57e13
Haruki <aratashiharuki@gmail.com>
2024-07-01 16:12:51 +0300

    First app component

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import Styles from '../styles/App.module.scss'
+
+export default () => {
+    return (
+        <div className={Styles.wrapper}>
+            <h1 className={Styles.title}>Бросай сюда</h1>
+
+            <div className={Styles.zonesHolder}>
+                
+            </div>
+        </div>
+    )
+}
